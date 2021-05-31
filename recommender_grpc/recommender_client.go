ab40af982f1a4dcefecde0a77dea738f2a20ffc6
Dmitry Shumov <dmitry.shumoff@gmail.com>
2021-06-01 00:02:25 +0300

    SMART-12: Expand API (#13)
    closes #12
    add router
    move api-schemas to separate file
    documentation refactoring
    new methods:
    + GET  /api/v1/movies
    + POST /api/v1/ratings/user/{user_id}/movie/{movie_id}
    + GET  /api/v1/users/{user_id}/profile
    + GET  /api/v1/users/{user_id}/movies
    + GET  /api/v1/recommendations/movie/{movie_id}
    + GET  /api/v1/recommendations/user/{user_id}/movie/{movie_id}
    
    TODO: /v1/movies/search - need to use elasticsearch

--- recommender_grpc/recommender_client.go
+++ recommender_grpc/recommender_client.go
@@ -2,8 +2,8 @@ package recommender_grpc
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc"
-	"log"
 	"time"
 
 	pb "github.com/shumoff/smart-theater-backend/protobuffs"
@@ -13,10 +13,12 @@ type RecommenderClient struct {
 	Address string
 }
 
-func (rc *RecommenderClient) GetRelevantMovies(userId int32, offset int32, limit int32) (movieIds []int32) {
+func (rc *RecommenderClient) GetRelevantMovies(userId int32, offset int32, limit int32) ([]int32, error) {
+	var movieIds []int32
+
 	conn, err := grpc.Dial(rc.Address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		return nil, fmt.Errorf("could not connect to recommender: %w", err)
 	}
 	defer conn.Close()
 	c := pb.NewRecommenderClient(conn)
@@ -25,7 +27,7 @@ func (rc *RecommenderClient) GetRelevantMovies(userId int32, offset int32, limit
 	defer cancel()
 	r, err := c.RecommendMovie(ctx, &pb.RelevantMovieRequest{UserId: userId, Offset: offset, Limit: limit})
 	if err != nil {
-		log.Fatalf("could not get recommendation: %v", err)
+		return nil, fmt.Errorf("could not get recommendation: %w", err)
 	}
 
 	relevantMovies := r.GetRecommendations()
@@ -33,5 +35,77 @@ func (rc *RecommenderClient) GetRelevantMovies(userId int32, offset int32, limit
 		movieIds = append(movieIds, relevantMovie.Id)
 	}
 
-	return movieIds
+	return movieIds, nil
+}
+
+func (rc *RecommenderClient) GetSimilarMovies(movieId int32, offset int32, limit int32) ([]int32, error) {
+	var movieIds []int32
+
+	conn, err := grpc.Dial(rc.Address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to recommender: %w", err)
+	}
+	defer conn.Close()
+	c := pb.NewRecommenderClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.RecommendSimilarMovie(ctx, &pb.SimilarMovieRequest{MovieId: movieId, Offset: offset, Limit: limit})
+	if err != nil {
+		return nil, fmt.Errorf("could not get similar movies: %w", err)
+	}
+
+	similarMovies := r.GetRecommendations()
+	for _, similarMovie := range similarMovies {
+		movieIds = append(movieIds, similarMovie.Id)
+	}
+
+	return movieIds, nil
+}
+
+func (rc *RecommenderClient) GetRelevantSimilarMovies(userId int32, movieId int32, offset int32, limit int32) ([]int32, error) {
+	var movieIds []int32
+
+	conn, err := grpc.Dial(rc.Address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to recommender: %w", err)
+	}
+	defer conn.Close()
+	c := pb.NewRecommenderClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.RecommendRelevantSimilarMovie(
+		ctx,
+		&pb.RelevantSimilarMovieRequest{UserId: userId, MovieId: movieId, Offset: offset, Limit: limit},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("could not get relevant similar movies: %w", err)
+	}
+
+	relevantSimilarMovies := r.GetRecommendations()
+	for _, relevantSimilarMovie := range relevantSimilarMovies {
+		movieIds = append(movieIds, relevantSimilarMovie.Id)
+	}
+
+	return movieIds, nil
+}
+
+func (rc *RecommenderClient) SaveNewRating(userId int32, movieId int32, rating int32) error {
+	conn, err := grpc.Dial(rc.Address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return fmt.Errorf("could not connect to recommender: %w", err)
+	}
+	defer conn.Close()
+	c := pb.NewRecommenderClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	_, err = c.SaveNewRating(ctx, &pb.NewRatingRequest{UserId: userId, MovieId: movieId, Rating: rating})
+	if err != nil {
+		return fmt.Errorf("could not save rating: %w", err)
+	}
+
+	return nil
 }
--- src/store.go
+++ src/store.go
@@ -11,9 +11,55 @@ type Store struct {
 	db *sql.DB
 }
 
-func (store *Store) GetMovieInfo(id int32) (*Movie, error) {
+func (s *Store) GetUserProfile(id int32) (*UserProfile, error) {
+	queryString := "SELECT id, username, photo_url from users WHERE id = $1;"
+	row := s.db.QueryRow(queryString, id)
+
+	userProfile := &UserProfile{}
+
+	err := row.Scan(&userProfile.UserId, &userProfile.Username, &userProfile.PhotoUrl)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("there is no such user: %w", err)
+		}
+		return nil, fmt.Errorf("could not get user from db: %w", err)
+	}
+
+	return userProfile, nil
+}
+
+func (s *Store) GetUserMovies(userId int32, pagination *Pagination) ([]*MoviePreview, error) {
+	queryString := `
+	select m.id, m.title, m.description, m.release_date, m.poster_url 
+	from ratings r
+	    join movies m on r.movie_id = m.id
+	where r.user_id = $1
+	offset $2 limit $3;
+	`
+
+	rows, err := s.db.Query(queryString, userId, pagination.Offset, pagination.Limit)
+	if err != nil {
+		return nil, fmt.Errorf("could not get user movies from db: %w", err)
+	}
+	defer rows.Close()
+
+	var userMovies []*MoviePreview
+
+	for rows.Next() {
+		movie := &MoviePreview{}
+		err := rows.Scan(&movie.MovieId, &movie.Title, &movie.Description, &movie.ReleaseDate, &movie.PosterUrl)
+		if err != nil {
+			return nil, fmt.Errorf("could not get movies from db: %w", err)
+		}
+		userMovies = append(userMovies, movie)
+	}
+
+	return userMovies, nil
+}
+
+func (s *Store) GetMovieInfo(id int32) (*Movie, error) {
 	queryString := "SELECT id, title, description, release_date, poster_url from movies WHERE id = $1;"
-	row := store.db.QueryRow(queryString, id)
+	row := s.db.QueryRow(queryString, id)
 
 	movie := &Movie{}
 
@@ -28,10 +74,58 @@ func (store *Store) GetMovieInfo(id int32) (*Movie, error) {
 	return movie, nil
 }
 
-func (store *Store) GetMoviesPreviewInfo(ids []int32) ([]*MoviePreview, error) {
-	queryString := "SELECT id, title, description, release_date, poster_url from movies WHERE id = any($1)"
+func (s *Store) RateMovie(userId int32, movieId int32, rating int32) error {
+	queryString := `
+	insert into ratings (user_id, movie_id, rating)
+	values ($1, $2, $3)
+	on conflict (user_id, movie_id) do update set rating = EXCLUDED.rating, timestamp = now()
+	`
+	_, err := s.db.Exec(queryString, userId, movieId, rating)
+	if err != nil {
+		return fmt.Errorf("unable to rate movie: %w", err)
+	}
 
-	rows, err := store.db.Query(queryString, pq.Array(ids))
+	return nil
+}
+
+func (s *Store) GetMoviesPreview(pagination *Pagination) ([]*MoviePreview, error) {
+	queryString := `
+	SELECT id, title, description, release_date, poster_url
+	from movies m
+	    join ratings r on m.id = r.movie_id
+	group by m.id
+	order by avg(r.rating) desc
+	offset $1 limit $2;
+	`
+
+	rows, err := s.db.Query(queryString, pagination.Offset, pagination.Limit)
+	if err != nil {
+		return nil, fmt.Errorf("could not get movies from db: %w", err)
+	}
+	defer rows.Close()
+
+	var movies []*MoviePreview
+
+	for rows.Next() {
+		movie := &MoviePreview{}
+		err := rows.Scan(&movie.MovieId, &movie.Title, &movie.Description, &movie.ReleaseDate, &movie.PosterUrl)
+		if err != nil {
+			return nil, fmt.Errorf("could not get movies from db: %w", err)
+		}
+		movies = append(movies, movie)
+	}
+
+	return movies, nil
+}
+
+func (s *Store) GetCertainMoviesPreview(ids []int32) ([]*MoviePreview, error) {
+	queryString := `
+	SELECT id, title, description, release_date, poster_url
+	from movies 
+	    join unnest($1::int[]) with ordinality t(id, ord) using (id)
+	order by t.ord;
+	`
+	rows, err := s.db.Query(queryString, pq.Array(ids))
 	if err != nil {
 		return nil, fmt.Errorf("could not get movies from db: %w", err)
 	}
